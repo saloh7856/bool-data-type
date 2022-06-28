4d4b34d3f1280fae17eade3c3942616904fb19df
saloh7856 <salohiddinberdiyorov2002@gmail.com>
2022-06-28 15:35:03 +0500

    if

---
+++ bin/1.dart
@@ -0,0 +1,13 @@
+void main(){
+    int x=0;
+    if(x>0){
+        print('pasitive');
+        }
+        if(x<0){
+    print('negative');
+        }
+    if(x==0){
+        print('zero');
+    }
+        
+}
